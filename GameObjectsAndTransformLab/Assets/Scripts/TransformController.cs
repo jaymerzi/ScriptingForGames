e34c3dabc2ee30dc17546f4aaba198b0c6092bc7
Jayden Wilkins <jayproducer1895@gmail.com>
2025-01-26 21:34:41 -0700

    Created GameObjectsAndTransformLab

---
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var x = Mathf.PingPong(Time.time, 3);
+        var p = new Vector3(0, x, 0);
+        transform.position = p;
+
+        //Transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+    }
+}
