5f98e97b0f34c09c891d7449cb1eedd6c5801e0d
Jayden Wilkins <jayproducer1895@gmail.com>
2025-01-26 21:48:35 -0700

    Completed GameObjectsAndTransformLab

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController1.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TransformController : MonoBehaviour
+public class TransformController1 : MonoBehaviour
 {
     // Start is called before the first frame update
     //void Start()
@@ -13,10 +13,11 @@ public class TransformController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        var x = Mathf.PingPong(Time.time, 3);
-        var p = new Vector3(-3, x, 0);
+        var x = Mathf.PingPong(Time.time, 1);
+        var p = new Vector3(x, x, 0);
         transform.position = p;
 
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 0, 30) * Time.deltaTime);
+
     }
 }
--- GameObjectsAndTransformLab/Assets/Scripts/TransformController2.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TransformController : MonoBehaviour
+public class TransformController2 : MonoBehaviour
 {
     // Start is called before the first frame update
     //void Start()
@@ -13,10 +13,11 @@ public class TransformController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        var x = Mathf.PingPong(Time.time, 3);
-        var p = new Vector3(-3, x, 0);
+        var x = Mathf.PingPong(Time.time, 4);
+        var p = new Vector3(3, 0, x);
         transform.position = p;
 
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(10, 5, 30) * Time.deltaTime);
+
     }
 }
