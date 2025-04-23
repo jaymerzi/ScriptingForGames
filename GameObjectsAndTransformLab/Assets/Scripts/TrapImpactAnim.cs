a12ea99e1ea17dd736102eaa068b4fd7882e120a
Jayden Wilkins <jayproducer1895@gmail.com>
2025-04-22 23:12:09 -0600

    Added some animations & broke prefabs

---
+++ GameObjectsAndTransformLab/Assets/Scripts/TrapImpactAnim.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapImpactAnim : MonoBehaviour
+{
+    public float growFactor = 1.4f;
+
+    // Start is called before the first frame update
+    public void Impact()
+    {
+        StartCoroutine(test());
+    }
+    IEnumerator test()
+    {
+        transform.localScale *= growFactor;
+        yield return new WaitForSeconds(0.1f);
+        transform.localScale /= growFactor;
+    }
+        /*transform.localScale *= growFactor;
+        yield return new WaitForSeconds(0.3);
+        transform.localScale /= growFactor;*/
+
+}
