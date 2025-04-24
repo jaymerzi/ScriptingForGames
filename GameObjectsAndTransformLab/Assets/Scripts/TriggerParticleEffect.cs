dd4ace116600482226daa40d3ccf67ae4942c206
Jayden Wilkins <jayproducer1895@gmail.com>
2025-04-24 13:05:39 -0600

    Scripting 10 Lab
    Added the first particle system

---
+++ GameObjectsAndTransformLab/Assets/Scripts/TriggerParticleEffect.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem), typeof(Collider))]
+public class TriggerParticleEffect : MonoBehaviour
+{
+    private ParticleSystem particleSystem; // Reference to the Particle System
+
+    public int firstEmissionAmount = 10;
+    public int secondEmissionAmount = 20;
+    public int thirdEmissionAmount = 30;
+    public float delayBetween = 0.2f;
+
+    public int particleAmount = 10; // Exposed variable for particle amount
+
+    private void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<CharacterController>()) // Check if the player triggered the event
+        {
+            particleSystem.Emit(particleAmount); // Emit the specified amount of particles
+        }
+    }
+
+    private IEnumerator EmitParticlesCoroutine()
+    {
+        particleSystem.Emit(firstEmissionAmount);
+        yield return new WaitForSeconds(delayBetween);
+
+        particleSystem.Emit(secondEmissionAmount);
+        yield return new WaitForSeconds(delayBetween);
+
+        particleSystem.Emit(thirdEmissionAmount);
+    }
+
+}
